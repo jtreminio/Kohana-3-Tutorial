b87fc387d5f348d35dc0d2b08f667096cfc136d2
Juan Treminio <jtreminio@gmail.com>
2012-03-21 18:55:49 -0500

    Controller_Welcome is now Controller_Home

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -1,10 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Home extends Controller {
 
 	public function action_index()
 	{
 		$this->response->body('hello, world!');
 	}
 
-} // End Welcome
+}
