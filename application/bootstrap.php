7fe92893fce22d0c8190eaa8b3bb2d8f6ac8aa18
Juan Treminio <jtreminio@gmail.com>
2012-03-21 18:07:27 -0500

    Routes go in their own file.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -111,11 +111,6 @@ Kohana::modules(array(
 	));
 
 /**
- * Set the routes. Each route must have a minimum of a name, a URI and a set of
- * defaults for the URI.
+ * Include separate routes file
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'welcome',
-		'action'     => 'index',
-	));
+require_once APPPATH.'config/routes.php';
