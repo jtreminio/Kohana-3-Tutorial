05df1bd1eabaf07759c6a7ffe4d3bfeb5e73df33
Juan Treminio <jtreminio@gmail.com>
2012-04-08 10:27:58 -0500

    Added view title.

--- application/classes/controller/home.php
+++ application/classes/controller/home.php
@@ -5,6 +5,7 @@ class Controller_Home extends Controller {
 	public function action_index()
 	{
 		$view = new View_Front_Home;
+		$view->title = 'This is our title!';
 		$this->response->body($view->render());
 	}
 
