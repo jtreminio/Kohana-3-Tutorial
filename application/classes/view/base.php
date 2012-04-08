8b559822fa1c1cbe24212d8cd1542b62137b634c
Juan Treminio <jtreminio@gmail.com>
2012-04-07 21:26:06 -0500

    Removed code not needed at this stage.

--- application/classes/view/base.php
+++ application/classes/view/base.php
@@ -1,11 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class View_Base extends Kostache
-{
-	public function __construct($template = NULL, array $partials = NULL)
-	{
-		$this->_partials[]= $partials;
-
-		parent::__construct($template, $this->_partials);
-	}
-}
+class View_Base extends Kostache{}
